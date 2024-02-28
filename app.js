f16be327b0606b0b363c9deac28fca84cef8cda8
baburali <baburali1910@gmail.com>
2024-02-28 03:58:52 -0800

    file created

---
+++ app.js
@@ -0,0 +1,37 @@
+// QUESTION .NO.1
+
+// alert("hey!");
+
+// var num = 300;
+
+// if(num % 3 === 0){
+//     console.log('number is divisible by 3')
+// }
+
+// else{
+// console.log('number is not divisible by 3')
+// }
+
+// Q NO.2
+
+// var num = 2501;
+
+// if(num % 2 === 0){
+//     console.log('even number')
+// }
+
+// else{
+//     console.log('odd number')
+// };
+
+// Q No.3
+
+// var num = 18;
+
+// if(num >= 18){
+//     console.log('old enough')
+// }
+// else{
+//     console.log('too young')
+// };
+
